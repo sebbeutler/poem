59d6104a43522bec17d65e450707ca6da310ef90
subski <seb.beutler@netcourrier.com>
2024-03-15 23:47:26 +0100

    init

---
+++ scrap.py
@@ -0,0 +1,54 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import sys
+import re
+import json
+import time
+
+def getSoup(url):
+    return BeautifulSoup(urllib.request.urlopen(url).read(), 'html.parser')
+
+index_page = getSoup("https://www.poesie-francaise.fr/poemes-auteurs/")
+author_links = index_page.select('.menu-centrale a')
+author_count = author_total = len(author_links)
+
+db: dict[str,list[str]] = {}
+
+for i, author_link in enumerate(author_links):
+    author_url = str(author_link.get('href')).replace('.fr/poemes-', '.fr/')
+    try:
+        if author_url == 'https://www.poesie-francaise.fr/':
+            author_count -= 1
+            continue
+
+        author = author_url.split('/')[-2]
+        author_page = getSoup(author_url)
+        db[author] = []
+
+        poem_links = author_page.select('article > .w3-panel > a')
+        poem_count = poem_total = len(poem_links)
+
+        for j, poem_link in enumerate(poem_links):
+            poem_url = str(poem_link.get('href'))
+            if not poem_url.startswith(author_url):
+                poem_count -= 1
+                continue
+            try:
+                poem_page = getSoup(poem_url)
+                poem = poem_page.select('#content > article > .w3-content > p')
+                if len(poem) == 0:
+                    continue
+                else:
+                    poem = poem[0]
+
+                db[author].append(poem.get_text(separator='\n'))
+                print(f'({i-(author_total-author_count)+1:4d}:{author_count:4d}){author}: [{j-(poem_total-poem_count)+1:4d}/{poem_count:4d}]', end='\r')
+            except:
+                print(f'Error {poem_url}')
+            # time.sleep(1)
+        print()
+    except:
+        print(f'Error {author_url}')
+
+with open('poemes.json', 'w') as jfile:
+    jfile.write(json.dumps(db))
